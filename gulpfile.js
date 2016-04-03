0877fe6d87eaa5c72d726b0c51cbd0a5b6188e22
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-04-03 14:36:13 -0500

    gulp configured

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify');
     
     
-    gulp.task('default', ['index']);
+    gulp.task('default', ['sassCombo','index','ugly','scss','watch']);
     
     
     gulp.task('index', function(){
@@ -14,4 +14,23 @@ var gulp = require('gulp'),
     
     gulp.task('watch', function(){
        gulp.watch('client/views/index.html',['index']); 
+       gulp.watch('client/scripts/*.js',['ugly']);
+       gulp.watch('client/styles/*.scss',['sassCombo','scss']);
+    });
+    
+    gulp.task('sassCombo', function(){
+       return gulp.src('client/styles/scss/*.scss')
+                .pipe(concat('style.scss'))
+                .pipe(gulp.dest('client/styles/')); 
+    });
+    
+    gulp.task('scss', function(){
+       return sass('client/styles/style.scss')
+                .pipe(gulp.dest('public/styles/')); 
+    });
+    
+    gulp.task('ugly', function(){
+       return gulp.src('client/scripts/*.js')
+                .pipe(uglify())
+                .pipe(gulp.dest('public/scripts/')); 
     });
